ee610fe38951361bc31d92367809004f3f391363
Chainatee Tanakulrungson <c.tanakul@gmail.com>
2017-10-11 12:07:54 -0500

    [plot the odom and vive tracking] can plot /odom now

--- src/test_odom.py
+++ src/test_odom.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
+import matplotlib.pyplot as plt
 import os
 import rosbag
 
 filename = "square_odom.bag"
 filepath = os.path.dirname(os.path.realpath(filename))
 bag = rosbag.Bag(filepath + '/bag/' + filename)
+xpoints = []
+ypoints = []
 for topic, msg, t in bag.read_messages(topics=['/odom']):
-    print msg
+    xpoints.append(msg.position.x)
+    ypoints.append(msg.position.y)
+    # print msg.position.x
+plt.plot(xpoints, ypoints, 'ro')
+
 bag.close()
+plt.show()
